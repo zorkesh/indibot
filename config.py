4e1574181464f0c53760b02f41d6bcba85256bdb
zorkesh <32952269+zorkesh@users.noreply.github.com>
2018-03-05 17:58:21 +0300

    Add files via upload
    Added "first look" logic on parsing Organosation record

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 token = '461232943:AAFJZRSq-tQjOFUn071M3oWp9RupYsXjH2I'
+indicatorUrl = 'https://indicator-api.bifit.com'
--- indicator.py
+++ indicator.py
@@ -1,16 +1,70 @@
 # -*- coding: utf-8 -*-
-import os
+import json
 
 import config
 import telebot
+import os
+import requests
+
 
 bot = telebot.TeleBot(config.token)
+os.environ["HTTPS_PROXY"] = 'https://istomin:zer4ty5@proxy.bifit.int:3128'
+os.environ["HTTP_PROXY"] = 'http://istomin:zer4ty5@proxy.bifit.int:3128'
 
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+def parseOrgrecord(orgRecord):
+    fullName = shortName = ''
+    ogrn = orgRecord['ogrn']
+    if 'fullName' in orgRecord:
+        fullName = orgRecord['fullName']
+    if 'shortName' in orgRecord:
+        shortName = orgRecord['shortName']
+    status = orgRecord['status']['statusName']
+    inn = orgRecord['inn']
+    address = ''
+    region = ''
+    if 'address' in orgRecord:
+        if 'index' in orgRecord['address']:
+            address += orgRecord['address']['index'] + ', '
+        if 'region' in orgRecord['address']:
+            if 'name' in orgRecord['address']['region']:
+                region += orgRecord['address']['region']['name'] + ' '
+            if 'type' in orgRecord['address']['region']:
+                region += orgRecord['address']['region']['type'] + ', '
+        if 'city' in orgRecord['address']:
+            if 'type' in orgRecord['address']['city']:
+                address += orgRecord['address']['city']['type'] + ' '
+            if 'name' in orgRecord['address']['city']:
+                address += orgRecord['address']['city']['name'] + ', '
+        if 'street' in orgRecord['address']:
+            if 'type' in orgRecord['address']['street']:
+                address += orgRecord['address']['street']['type'] + ' '
+            if 'name' in orgRecord['address']['street']:
+                address += orgRecord['address']['street']['name'] + ', '
+        if 'house' in orgRecord['address']:
+            address += orgRecord['address']['house']
+        if 'apartment' in orgRecord['address']:
+            address += ', ' + orgRecord['address']['apartment']
+    return "ОГРН: " + ogrn + "\n" + "ИНН: " + inn + "\n" + "Краткое наименование: " + shortName + "\n" + \
+          "Полное наименование: " + fullName + "\n" + "Адрес: " + address + "\n" + "Статус: " + status
+
+
+
+@bot.message_handler(regexp=r'\d{10}')
+def handle_message(message):
+    payload = {'inn': message.text, 'installation_id': 9999}
+    rorg = requests.get(config.indicatorUrl + '/api/v0/orginfo/organizations/record', params=payload, verify=False)
+    if rorg.text:
+        orginforecord = json.loads(rorg.text)
+    else:
+        orginforecord = json.loads(
+            "{\"errorCode\":2000,\"inn\":\"25\",\"errorDescriptionRu\":\"Контрагент не найден\","
+            "\"errorDescriptionEn\":\"Contractor is not found\"}")
+    if not 'errorDescriptionRu' in orginforecord:
+        bot.send_message(message.chat.id, parseOrgrecord(orginforecord))
+    else:
+        bot.send_message(message.chat.id, orginforecord["errorDescriptionRu"])
 
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
