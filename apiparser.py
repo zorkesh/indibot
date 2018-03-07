d93e10f2a6c8bd2eb3d6515457b9eee96737a0a6
zorkesh <32952269+zorkesh@users.noreply.github.com>
2018-03-07 16:11:58 +0300

    Add files via upload
    разбор индивидуальных предпринимателей
    доп. информация по организациям.

---
+++ apiparser.py
@@ -0,0 +1,223 @@
+import json
+from decimal import Decimal
+
+import requests
+
+import config
+import tools
+"""
+Модуль для разбора данных ответа API Индикатора.
+get* - получение JSON объекта из API
+parse* - разбор JSON объекта и возврат текста для сообщения бота 
+"""
+
+ratings = {'green': u'\U0001F4D7', 'red': u'\U0001F4D5', 'blue': u'\U0001F4D8', 'yellow': u'\U0001F4D9'}
+numbers = [u'\U00000030'+u'\U000020E3', u'\U00000031'+u'\U000020E3', u'\U00000032'+u'\U000020E3',
+           u'\U00000033'+u'\U000020E3', u'\U00000034'+u'\U000020E3', u'\U00000035'+u'\U000020E3',
+           u'\U00000036'+u'\U000020E3', u'\U00000037'+u'\U000020E3', u'\U00000038'+u'\U000020E3',
+           u'\U00000039'+u'\U000020E3']
+
+
+def getMainInfo(inn):
+    payload = {'inn': inn, 'installation_id': 9999}
+    result = requests.get(config.indicatorUrl + '/api/v0/orginfo/organizations/record', params=payload, verify=config.verification)
+    if result.text:
+        data = json.loads(result.text)
+    else:
+        data = json.loads(
+            "{\"errorCode\":2000,\"inn\":\"25\",\"errorDescriptionRu\":\"Контрагент не найден\","
+            "\"errorDescriptionEn\":\"Contractor is not found\"}")
+    return data
+
+
+def getLeaders(inn):
+    payload = {'inn': inn, 'installation_id': 9999}
+    result = requests.get(config.indicatorUrl + '/api/v0/orginfo/leaders/list', params=payload, verify=config.verification)
+    if result.text:
+        data = json.loads(result.text)
+    else:
+        data = json.loads(
+            "{\"errorCode\":2000,\"inn\":\"25\",\"errorDescriptionRu\":\"Информации о лидерах нет\","
+            "\"errorDescriptionEn\":\"Contractor is not found\"}")
+    return data
+
+
+def getFounders(ogrn):
+    payload = {'inn': ogrn, 'installation_id': 9999}
+    result = requests.get(config.indicatorUrl + '/api/v1/entities/' + ogrn + '/stakes', params=payload, verify=config.verification)
+    data = json.loads(result.text)
+    if not data['content']:
+        data = json.loads(
+            "{\"errorCode\":2000,\"inn\":\"25\",\"errorDescriptionRu\":\"Информации об учредителях нет\","
+            "\"errorDescriptionEn\":\"Contractor is not found\"}")
+    return data
+
+
+def getFinanceSummary(okpo):
+    result = requests.get(config.indicatorUrl + '/api/v1/bookkeeping/' + okpo + '/summary', verify=config.verification)
+    data = json.loads(result.text)
+    if not data['content']['data']:
+        data = json.loads(
+            "{\"errorCode\":2000,\"inn\":\"25\",\"errorDescriptionRu\":\"Бухгалтерская отчетность не опубликована\","
+            "\"errorDescriptionEn\":\"Contractor is not found\"}")
+    return data
+
+
+def getRating(inn):
+    payload = {'inn': inn, 'installation_id': 9999}
+    result = requests.get(config.indicatorUrl + '/rating', params=payload, verify=config.verification)
+    return json.loads(result.text)
+
+
+def parsemaincodes(orgRecord):
+    ogrn = okpo = ''
+    ogrn = orgRecord['ogrn']
+    if 'okpo' in orgRecord:
+        okpo = orgRecord['okpo']
+    return ogrn, okpo
+
+
+def parseRating(orgRecord):
+    message = 'Краткая информация о '
+    kpp = ''
+    if 'kpp' in orgRecord:
+        kpp = orgRecord['kpp']
+    if 'shortName' in orgRecord:
+        shortName = orgRecord['shortName']
+    else:
+        shortName = fullName = orgRecord['fullName']
+    status = orgRecord['status'].capitalize()
+    inn = orgRecord['inn']
+    rInd = int(orgRecord['criticalFacts'])
+    yInd = int(orgRecord['payAttentionFacts'])
+    gInd = int(orgRecord['activityFacts'])
+    bInd = int(orgRecord['achievements'])
+    indicators = ratings['red'] + numbers[rInd] + ratings['yellow'] + numbers[yInd] + ratings['green'] + numbers[gInd] + ratings['blue'] + numbers[bInd]
+    message += shortName + "\n\n"
+    message += indicators + "\n\n"
+    message += "*Статус:* " + status + "\n"
+    message += "*ИНН:* " + inn + "\n"
+    if not kpp == '':
+        message += "*КПП:* " + kpp
+
+    return message
+
+
+def parseOrgrecord(orgRecord):
+    capital = ''
+    mainActivity = ''
+    message = ''
+    fullName = shortName = ''
+    ogrn = orgRecord['ogrn']
+    # if 'fullName' in orgRecord:
+    #     fullName = orgRecord['fullName']
+    if 'shortName' in orgRecord:
+        shortName = orgRecord['shortName']
+    else:
+        shortName = fullName = orgRecord['fullName']
+    status = orgRecord['status']['statusName'].capitalize()
+    inn = orgRecord['inn']
+    if 'registrationDate' in orgRecord:
+        regdate = orgRecord['registrationDate']
+    else:
+        regdate = orgRecord['ogrnDate']
+    address = ''
+    if 'address' in orgRecord:
+        if 'index' in orgRecord['address']:
+            address += orgRecord['address']['index'] + ', '
+        if 'region' in orgRecord['address']:
+            if 'type' in orgRecord['address']['region']:
+                address += orgRecord['address']['region']['type'].lower() + ' '
+            if 'name' in orgRecord['address']['region']:
+                address += orgRecord['address']['region']['name'].capitalize() + ', '
+        if 'city' in orgRecord['address']:
+            if 'type' in orgRecord['address']['city']:
+                address += orgRecord['address']['city']['type'].lower() + ' '
+            if 'name' in orgRecord['address']['city']:
+                address += orgRecord['address']['city']['name'].capitalize() + ', '
+        if 'street' in orgRecord['address']:
+            if 'type' in orgRecord['address']['street']:
+                address += orgRecord['address']['street']['type'].lower() + ' '
+            if 'name' in orgRecord['address']['street']:
+                address += orgRecord['address']['street']['name'].capitalize() + ', '
+        if 'house' in orgRecord['address']:
+            address += orgRecord['address']['house'].lower()
+        if 'apartment' in orgRecord['address']:
+            address += ', ' + orgRecord['address']['apartment'].lower()
+    if 'capitalValue' in orgRecord:
+        capital = tools.moneyfmt(Decimal(orgRecord['capitalValue']))
+    if 'mainEconomicActivity' in orgRecord:
+        mainActivity = orgRecord['mainEconomicActivity']['code'] + ' ' + orgRecord['mainEconomicActivity']['name']
+
+    message += "*ОГРН:* " + ogrn + "\n"
+    message += "*Дата создания: *" + regdate + "\n"
+    message += "*Адрес:* " + address + "\n"
+    if not capital == '':
+        message += "*Уставный капитал: *" + capital + " руб." + "\n"
+    message += "*Основной вид деятельности: *" + mainActivity + "\n"
+    return message
+
+
+def parseLeaders(orgRecord):
+    message = ''
+    if not 'errorDescriptionRu' in orgRecord:
+        for auth_data in orgRecord:
+            if auth_data['patronymic']:
+                patr = auth_data['patronymic']
+            else:
+                patr = ''
+            fio = auth_data['surname'].capitalize() + ' ' + auth_data['firstName'].capitalize() + ' ' + patr.capitalize()
+            # fio = re.sub(u'[Ёё]', u'Е', fio)
+            position = auth_data['position']
+            message += '*' + position.capitalize() + ':* '
+            message += fio + "\n"
+    else:
+        message = orgRecord['errorDescriptionRu']
+    return message
+
+
+def parseFounders(orgRecord):
+    message = '*Учредители: *\n'
+    if not 'errorDescriptionRu' in orgRecord:
+        for founder_data in orgRecord['content']:
+            if not founder_data['content']['stakeOwner']['type'] == 'PERSON':
+                name = founder_data['content']['stakeOwner']['fullName']
+            else:
+                if founder_data['content']['stakeOwner']['patronymic']:
+                    patr = founder_data['content']['stakeOwner']['patronymic']
+                else:
+                    patr = ''
+                name = founder_data['content']['stakeOwner']['surname'].capitalize() + ' ' + \
+                       founder_data['content']['stakeOwner']['name'].capitalize() + ' ' + patr.capitalize()
+            if 'inn' in founder_data['content']['stakeOwner']:
+                inn = founder_data['content']['stakeOwner']['inn']
+            else:
+                inn = ''
+            stake = founder_data['content']['stakeSize']
+            try:
+                stake_size = tools.moneyfmt(Decimal(stake)) + ' руб.'
+            except Exception:
+                stake_size = str(stake)
+            message += u'\U000025CF' + str(name) + ' - ' + stake_size + '\n'
+            if not inn == '':
+                message += '(ИНН: ' + inn + ')' + '\n'
+    else:
+        message = orgRecord['errorDescriptionRu'] + '\n'
+    return message
+
+
+def parseFinSummary(orgRecord):
+    message = '*Бухгалтерская отчетность за '
+    if not 'errorDescriptionRu' in orgRecord:
+        year = sorted(list(orgRecord['content']['data'].keys()))[-1]
+        message += year + " год.*" + "\n"
+        lastdata = orgRecord['content']['data'][year]
+        message += "*Баланс: *" + tools.moneyfmt(Decimal(lastdata['actives'])) + " руб.\n"
+        message += "*Выручка: *" + tools.moneyfmt(Decimal(lastdata['earnings'])) + " руб.\n"
+        if Decimal(lastdata['profit']) >= 0:
+            message += "*Прибыль: *" + tools.moneyfmt(Decimal(lastdata['profit'])) + " руб.\n"
+        else:
+            message += "*Убыток: *" + tools.moneyfmt(Decimal(lastdata['profit']), neg='') + " руб.\n"
+    else:
+        message = "*" + orgRecord['errorDescriptionRu'] + "*"
+    return message
--- indicator.py
+++ indicator.py
@@ -1,69 +1,115 @@
 # -*- coding: utf-8 -*-
 import json
 
+import re
+
+import apiparser
 import config
 import telebot
+from telebot import types
 import os
 import requests
 
 
 bot = telebot.TeleBot(config.token)
-os.environ["HTTPS_PROXY"] = 'https://istomin:zer4ty5@proxy.bifit.int:3128'
-os.environ["HTTP_PROXY"] = 'http://istomin:zer4ty5@proxy.bifit.int:3128'
 
 
-def parseOrgrecord(orgRecord):
-    fullName = shortName = ''
-    ogrn = orgRecord['ogrn']
-    if 'fullName' in orgRecord:
-        fullName = orgRecord['fullName']
-    if 'shortName' in orgRecord:
-        shortName = orgRecord['shortName']
-    status = orgRecord['status']['statusName']
-    inn = orgRecord['inn']
-    address = ''
-    region = ''
-    if 'address' in orgRecord:
-        if 'index' in orgRecord['address']:
-            address += orgRecord['address']['index'] + ', '
-        if 'region' in orgRecord['address']:
-            if 'name' in orgRecord['address']['region']:
-                region += orgRecord['address']['region']['name'] + ' '
-            if 'type' in orgRecord['address']['region']:
-                region += orgRecord['address']['region']['type'] + ', '
-        if 'city' in orgRecord['address']:
-            if 'type' in orgRecord['address']['city']:
-                address += orgRecord['address']['city']['type'] + ' '
-            if 'name' in orgRecord['address']['city']:
-                address += orgRecord['address']['city']['name'] + ', '
-        if 'street' in orgRecord['address']:
-            if 'type' in orgRecord['address']['street']:
-                address += orgRecord['address']['street']['type'] + ' '
-            if 'name' in orgRecord['address']['street']:
-                address += orgRecord['address']['street']['name'] + ', '
-        if 'house' in orgRecord['address']:
-            address += orgRecord['address']['house']
-        if 'apartment' in orgRecord['address']:
-            address += ', ' + orgRecord['address']['apartment']
-    return "ОГРН: " + ogrn + "\n" + "ИНН: " + inn + "\n" + "Краткое наименование: " + shortName + "\n" + \
-          "Полное наименование: " + fullName + "\n" + "Адрес: " + address + "\n" + "Статус: " + status
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    resultstart = "Здравствуйте. \n" \
+             "Я бот для получения информации о юридических лицах " \
+                  "и индивидуальных предпринимателях из сервиса \"Индикатор\"\n" \
+             "Пока только о юридических лицах =)\n" \
+             "Для поиска введите 10 значный ИНН \n" \
+                  "Удачи "
+    bot.send_message(message.chat.id, resultstart)
 
+
+@bot.message_handler(regexp=r'\d{12}')
+def handle_ip_message(message):
+    try:
+        inn = message.text
+        rate = apiparser.getRating(inn)
+        if not 'errorDescriptionRu' in rate:
+            respmessage = ''
+            ratemessage = apiparser.parseRating(rate)
+            respmessage += ratemessage + "\n"
+            orginforecord = apiparser.getMainInfo(inn)
+            # Ищем организацию
+            mainInfo = apiparser.parseOrgrecord(orginforecord)
+            respmessage += mainInfo + "\n"
+            bot.send_message(message.chat.id, respmessage, parse_mode='markdown')
+        else:
+            bot.send_message(message.chat.id, rate["errorDescriptionRu"])
+    except Exception as e:
+        bot.send_message(message.chat.id, 'Что-то пошло не так. Попробуйте позже\n' + str(e))
 
 
 @bot.message_handler(regexp=r'\d{10}')
 def handle_message(message):
-    payload = {'inn': message.text, 'installation_id': 9999}
-    rorg = requests.get(config.indicatorUrl + '/api/v0/orginfo/organizations/record', params=payload, verify=False)
-    if rorg.text:
-        orginforecord = json.loads(rorg.text)
+    try:
+        inn = message.text
+        rate = apiparser.getRating(inn)
+        if not 'errorDescriptionRu' in rate:
+            respmessage = ''
+            ratemessage = apiparser.parseRating(rate)
+            respmessage += ratemessage + "\n"
+            orginforecord = apiparser.getMainInfo(inn)
+            ogrn, okpo = apiparser.parsemaincodes(orginforecord)
+            # Ищем организацию
+            mainInfo = apiparser.parseOrgrecord(orginforecord)
+            respmessage += mainInfo + "\n"
+            # Ищем директоров
+            leadersInfo = apiparser.getLeaders(inn)
+            respmessage += apiparser.parseLeaders(leadersInfo) + "\n"
+            # TODO Добавить учредителей
+            foundersInfo = apiparser.getFounders(ogrn)
+            respmessage += apiparser.parseFounders(foundersInfo) + "\n"
+            # Ищем бух.отчетнсть
+            if okpo == '':
+                respmessage += "*Бухгалтерская отчетность не опубликована*"
+            else:
+                finInfo = apiparser.getFinanceSummary(okpo)
+                respmessage += apiparser.parseFinSummary(finInfo) + "\n"
+            bot.send_message(message.chat.id, respmessage, parse_mode='markdown')
+        else:
+            bot.send_message(message.chat.id, rate["errorDescriptionRu"])
+    except Exception as e:
+        bot.send_message(message.chat.id, 'Что-то пошло не так. Попробуйте позже\n')
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_msg(message):
+    bot.send_message(message.chat.id, "Я пока не понимаю никаких сообщений кроме ИНН :(")
+
+
+@bot.inline_handler(func=lambda query: len(query.query) > 0)
+def query_text(query):
+    pattern = re.compile(r'\d{10}', re.MULTILINE)
+    try:
+        matches = re.match(pattern, query.query)
+        if not matches:
+            return
+    # Вылавливаем ошибку, если вдруг юзер ввёл чушь
+    # или задумался после ввода первого числа
+    except AttributeError:
+        return
+    inn = matches.string
+    org = apiparser.getRating(inn)
+    if not 'errorDescriptionRu' in org:
+        r_inf = types.InlineQueryResultArticle(id=1,
+                                               title=inn,
+                                               description=org['shortName'] if 'shortName' in org else org['fullName'],
+                                               input_message_content=types.InputTextMessageContent(message_text=inn,
+                                                                                                   parse_mode='markdown'))
+        bot.answer_inline_query(query.id, [r_inf])
     else:
-        orginforecord = json.loads(
-            "{\"errorCode\":2000,\"inn\":\"25\",\"errorDescriptionRu\":\"Контрагент не найден\","
-            "\"errorDescriptionEn\":\"Contractor is not found\"}")
-    if not 'errorDescriptionRu' in orginforecord:
-        bot.send_message(message.chat.id, parseOrgrecord(orginforecord))
-    else:
-        bot.send_message(message.chat.id, orginforecord["errorDescriptionRu"])
+        r_inf = types.InlineQueryResultArticle(id=1,
+                                               title=inn,
+                                               description="Контрагент не найден",
+                                               input_message_content=types.InputTextMessageContent(message_text=inn,
+                                                                                                   parse_mode='markdown'))
+        bot.answer_inline_query(query.id, [r_inf])
 
 
 if __name__ == '__main__':
