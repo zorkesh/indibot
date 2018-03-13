82d294072c5de337517d1c14e306219efc94ebfa
zorkesh <32952269+zorkesh@users.noreply.github.com>
2018-03-13 11:37:44 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+token = '518480616:AAHOABeSXWQu4n-Z8VoCKhv2-fKS6YRAIXc'
+indicatorUrl = 'https://indicator-api.bifit.com'
+os.environ["HTTPS_PROXY"] = 'https://istomin:zer4ty5@proxy.bifit.int:3128'
+os.environ["HTTP_PROXY"] = 'http://istomin:zer4ty5@proxy.bifit.int:3128'
+verification = False
