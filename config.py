46fb33633cb242d1954dd9bd2563eb8dff14b51c
zorkesh <32952269+zorkesh@users.noreply.github.com>
2018-03-21 13:47:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ token = '518480616:AAHOABeSXWQu4n-Z8VoCKhv2-fKS6YRAIXc'
 indicatorUrl = 'https://indicator-api.bifit.com'
 #os.environ["HTTPS_PROXY"] = 'https://istomin:zer4ty5@proxy.bifit.int:3128'
 #os.environ["HTTP_PROXY"] = 'http://istomin:zer4ty5@proxy.bifit.int:3128'
-verification = False
+verification = True
